308aca375c58869fafdb1c0d1e88ba9864950701
master <ijetsoft@gmail.com>
2023-05-01 17:52:16 +0300

    err1

--- src/routes/db1/+page.server.js
+++ src/routes/db1/+page.server.js
@@ -1,4 +1,4 @@
-import { supabase } from "$lib/supabaseClient";
+import { supabase } from "$lib/supabaseClient.js";
 
   export async function load() {
     const { data } = await supabase.from("countries").select();
