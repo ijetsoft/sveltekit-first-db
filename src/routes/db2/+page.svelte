bfd8d2d73f941e9ff89bf059f88e43234b01d1a3
master <ijetsoft@gmail.com>
2023-05-01 18:03:59 +0300

    plud db2

---
+++ src/routes/db2/+page.svelte
@@ -0,0 +1,16 @@
+﻿<script>
+    export let data;
+    let { Products } = data;
+    $: ({ Products } = data);
+  </script>
+
+  <table style="width: 50%; margin-top: 50px;">
+    <tr><th>название</th> <th>упаковка</th><th>цена</th></tr>
+    {#each Products as prod}
+    <tr><td>{prod.ProductName}</td> <td>{prod.QuantityPerUnit}</td> <td>{prod.UnitPrice}</td></tr>
+    {/each}
+  </table>
+
+  <style>
+    
+  </style>
