8af56ac0f5fd52df5efcb9e3b7624a160cf70437
master <ijetsoft@gmail.com>
2023-05-04 09:04:17 +0300

    4-05 9-01

--- src/routes/db/dbCategories/+page.svelte
+++ src/routes/db/dbCategories/+page.svelte
@@ -7,7 +7,9 @@
   <table style="width: 30%; margin-top: 50px;">
     <tr><th>название</th> <th>описание</th></tr>
     {#each categories as category}
-      <tr><td>{category.CategoryName}</td> <td>{category.Description}</td></tr>
+      <tr>
+        <td>*{category.CategoryName}</td> 
+        <td>{category.Description}</td></tr>
     {/each}
   </table>
 
