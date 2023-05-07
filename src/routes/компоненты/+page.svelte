013b8cbdcef4ca0f86d7c81dbae04a88d57a1eb1
master <ijetsoft@gmail.com>
2023-05-07 20:59:04 +0300

    07-05 5-1

---
+++ src/routes/компоненты/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+   
+</script>
+<div class="vertical-menu">
+<a href="../components/счетчик" class="active">Счетчик</a>
+<a href="../components/комбобокс">комбобокс</a>
+<a href="../db2">Скринсейвер</a>
+</div>
+
+
+<style>
+  .vertical-menu {
+  width: 200px; /* Set a width if you like */
+  margin-top: 50px;
+}
+
+.vertical-menu a {
+background-color: #eee; /* Grey background color */
+color: black; /* Black text color */
+display: block; /* Make the links appear below each other */
+padding: 12px; /* Add some padding */
+text-decoration: none; /* Remove underline from links */
+}
+
+.vertical-menu a:hover {
+background-color: #ccc; /* Dark grey background on mouse-over */
+}
+
+.vertical-menu a.active {
+background-color: #04AA6D; /* Add a green color to the "active/current" link */
+color: white;
+}
+</style>
---
+++ src/routes/компоненты/комбобокс/components/Combo.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let options = ['item1', 'item2', 'item3', 'item4'];
+	//export let displayText = a => a.text;
+	let name = 'world';
+	export let defaultOnChange = onChange
+	function onChange() {
+		// console.log(event.target.value);
+}
+</script>
+
+<select class="seazon" on:change={defaultOnChange}>
+	{#each options as option, i}
+		<option value={i}>{option}</option>
+	{/each}
+     
+  </select>
+<style>
+select {
+padding: 5px 100px 5px 5px; 
+	/* 100px required to make sure image displays */
+font-size: 16px;
+border: 1px solid #19247c;
+height: 34px;
+border-radius: 10px;
+-webkit-appearance: none;
+-moz-appearance: none;
+appearance: none;
+background: url("./icon.png") 96% / 15% no-repeat #9393ff;
+}
+</style>
