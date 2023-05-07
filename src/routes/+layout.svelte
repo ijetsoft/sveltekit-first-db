308a31672f8e7abca2828a78c9bcf66c8438d53d
master <ijetsoft@gmail.com>
2023-05-07 19:59:02 +0300

    07-05 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,7 @@
 		<slot />
 	</main>
 
-	<footer>
-		
-	</footer>
+
 </div>
 
 <style>
@@ -33,21 +31,9 @@
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
+	
 
 	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,15 +38,7 @@
 	display: inline-block; 
 	text-align: left; 
 	}
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
 	
-
 	h1 {
 		width: 100%;
 	}
