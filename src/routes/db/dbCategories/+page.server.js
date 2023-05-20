cb15f2ecb2e748e885e00e3353b2d1a51688f995
master <ijetsoft@gmail.com>
2023-05-20 19:57:12 +0300

    20-05 1

--- src/routes/db/dbCategories/+page.server.js
+++ src/routes/db/dbCategories/+page.server.js
@@ -1,14 +1,8 @@
 import { supabase } from "$lib/supabaseClient.js";
 
   export async function load() {
-    let { data } : any = {};
-    let x = await supabase.from("Category").select();
-    let y = await supabase.from("Product").select();
-     data  = {x, y};
-    //const { data } = await supabase.from('select "Id" AS _Key, "CategoryName" AS _Value from "Category" as CategoryId').select();
+    const { data } = await supabase.from("Category").select();
     return {
-      categories: data1 ?? [],
-      CategoryID: data ?? [],
-
+      categories: data ?? [],
     };
   }
--- src/routes/db/dbProducts/+page.server.ts
+++ src/routes/db/dbProducts/+page.server.ts
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient.js";
 
   export async function load() {
-    let { data } : any = {}
+    let data : any = {}
     let product = await supabase.from("Product").select();
     let category = await supabase.from("Category").select("Id, CategoryName");
     let suppliers = await supabase.from("Supplier").select("Id, CompanyName");    
