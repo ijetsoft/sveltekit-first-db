80bb5e5cd0dea91a2bf5f1dd0d6c01efea09a1c5
master <ijetsoft@gmail.com>
2023-08-15 09:47:14 +0300

    15.08 h

--- src/routes/components/JetDBTable/JetDBTable2.svelte
+++ src/routes/components/JetDBTable/JetDBTable2.svelte
@@ -491,6 +491,7 @@ function appendTD(parmTR: HTMLTableRowElement, parmTD: HTMLTableCellElement) {
 }
 
 function sortGridDoIt(colNum:number, sortMode:string) {
+  alert( window.innerWidth+' '+window.innerHeight)
   let type = thisCol[colNum-1].type
   let rowsArray = Array.from(tBody.rows);
   let compare
@@ -553,7 +554,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
 
 <!-- <div class="loader"></div> -->
 
-<section id="parentbox">
+<section class="panel_navi">
   <button class="navibtn" title="первая запись" on:click={myFirst}>
     {@html getSVG('FirstRecord', 'Gold')}</button>
   <!-- <button class="navibtn" title="первая запись" on:click={myFirst}>
@@ -582,7 +583,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
 <button class="navibtn" title="удалить запись" on:click={deleteRecord}>
   <svg fill="yellow" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
 </button>
-<div class="div_version" >версия 14.08 w</div>
+<div class="div_version" >версия 15.08 h</div>
 
 <!-- <p class="boring-text" data-dir="asc">Here is some plain old boring text.</p> -->
 </section>
@@ -642,7 +643,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
   ></Dialog>
    <!-- bind:RetDialog={RetTable}  -->
 <style>
-
+  .panel_navi {display: inline-block;}
   .loader {
   border: 16px solid #f3f3f3;
   border-radius: 50%;
@@ -664,6 +665,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
   float: right;
 }
 .navi_input {
+  
   float: left; 
   margin-top: 2px;
   max-width: 60px; 
@@ -675,6 +677,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
   /* transform: rotate(270deg); */
 }
 .navibtn {
+  float: left;
   background-color: FireBrick;
   border: none;
   color: Gold;
@@ -682,7 +685,7 @@ function sortGridDoIt(colNum:number, sortMode:string) {
   cursor: pointer;
   padding: 3px 4px;
   border-radius: 5px;
-  float: left;
+
   margin: 2px;
 }
 tr:nth-child(even) {
